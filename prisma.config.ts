65b6b484a7597a1922488b7999c54a241679b8d7
Hisyam <sehunbolt@gmail.com>
2026-06-12 15:16:42 +0700

    p

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "prisma/config";
+import { env } from "process";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+
+  migrations: {
+    path: "prisma/migrations",
+  },
+
+  datasource: {
+    url: env("DATABASE_URL"),
+    directUrl: env("DIRECT_URL"),
+  },
+});
